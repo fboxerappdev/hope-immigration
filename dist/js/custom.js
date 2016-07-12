508dcdd898face743af3b6272f0271ecc2ab3348
Sukhjinder <freezingfboxer@gmail.com>
2016-07-12 17:14:54 +0530

    index page responsive done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     $('.bxslider').bxSlider({
         auto:true,
     });
+    $('.testimonial-slide').bxSlider({
+        auto:true,
+        pager:false,
+        slideMargin:10
+    });
+
+    $(".nav-header span").click(function(){
+        $(".navbar-full ul").stop(true,false).slideToggle();
+    });
+
 });
